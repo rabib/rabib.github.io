8fefc341584fcd41568a23a50be6df4a2ebfcd14
rabib <53358432+rabib@users.noreply.github.com>
2021-04-17 23:59:20 -0400

    index text

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,11 +13,17 @@ const IndexPage = () => (
   <Layout activePage="/">
     <SEO />
     <div className="mt-12 flex flex-col-reverse lg:flex-row items-center lg:justify-between lg:space-x-6">
-      <h1 className="mt-12 lg:mt-0 max-w-3xl text-3xl sm:text-4xl text-primary font-semibold sm:text-left md:text-center lg:text-left">
-        Hey, I’m Rabib.
-        <br className="mb-6 lg:hidden" /> 
-         I'm interested in cryptography and data science.
-      </h1>
+      <div>
+        <h1 className="mt-12 lg:mt-0 max-w-3xl text-3xl sm:text-4xl text-primary font-semibold sm:text-left md:text-center lg:text-left">
+          Rabib:
+          <br className="mb-6 lg:hidden" /> Cryptographer and Data Scientist
+        </h1>
+        <div className="relative text-primary">
+          <p className="mt-6">My research interests are quantum cryptography and zero-knowledge proofs. I am also very interested in distributed ledgers and decentralized finance. I currently work as a Data Scientist at Health Canada.</p>
+          <p className="mt-2">I hold an MSc in Mathematics from the University of Ottawa, where I also did my undergraduate studies. I've held multiple positions in government, including a few research assistantships with the Multilingual Text Processing group at the National Research Council, where I worked with their statistical machine translation system, PORTAGE.</p>
+
+        </div>
+      </div>
       <div className="relative">
         <Blob />
         <div className="absolute w-full h-full top-0 flex items-center justify-center">
